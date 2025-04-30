01cb8675d9ce651e77c063aac14e356920114f79
ranatuhin2 <aimlesstuhin@gmail.com>
2025-05-01 00:23:47 +0530

    Added PhpStan and And added Route for Product CRUD

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::group(['prefix'=>'product'], function(){
+    Route::get('/', [ProductController::class, 'index']);
+    Route::post('/create', [ProductController::class, 'store']);
+    Route::get('/edit/{product}', [ProductController::class, 'show']);
+    Route::put('/update/{product}', [ProductController::class, 'update']);
+    Route::delete('/delete/{product}', [ProductController::class, 'destroy']);
 });
 
 
